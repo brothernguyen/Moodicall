bd5b880bacfb892a9235d2c737cac4d80dd93706
Anh Nguyen <anh.tuan.nguyen@terralogic.com>
2018-06-27 21:22:52 +0700

    Search btn

--- Moody/HomeViewController.swift
+++ Moody/HomeViewController.swift
@@ -11,15 +11,20 @@ import UIKit
 class HomeViewController: UIViewController {
     
     @IBOutlet weak var viewNowBtn: UIButton!
+    @IBOutlet weak var searchBtn: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         viewNowBtn.layer.cornerRadius = 5
+        searchBtn.layer.cornerRadius = 5
     }
     
     @IBAction func viewNowBtnPressed(_ sender: UIButton) {
         
+    }
+    
+    @IBAction func searchBtnPressed(_ sender: UIButton) {
     }
     
 }
