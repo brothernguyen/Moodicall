8ddcc07280702bf45014983a3552530dada61a3e
Anh Nguyen <anh.tuan.nguyen@terralogic.com>
2018-06-27 16:36:04 +0700

    Added View Now btn

--- Moody/HomeViewController.swift
+++ Moody/HomeViewController.swift
@@ -9,17 +9,18 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var viewNowBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        viewNowBtn.layer.cornerRadius = 5
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func viewNowBtnPressed(_ sender: UIButton) {
+        
     }
-
-
+    
 }
 
